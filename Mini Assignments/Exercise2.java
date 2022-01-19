eec503bf97218c4c18e2005338352ece7ab75a71
Veniotn <w0404614@nscc.ca>
2022-01-19 17:26:52 -0400

    Uploading Exercise 2

---
+++ Mini Assignments/Exercise2.java
@@ -0,0 +1,13 @@
+public class Exercise2 {
+	 public static void main(String []args) {
+		 int value1 = 20;
+		 int value2 = 5;
+		 int answer = divide(value1,value2);
+		 System.out.println("dividing " + value1 + " by " + value2 + " returned " + answer);
+		// Here is where my cool code will go.
+	 }
+	 public static int divide(int value1, int value2) {
+	 		int result = value1 / value2;
+	 		return result;
+	 }
+}
