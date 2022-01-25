693ed08f0a76812be61cc3d8a6813fa55f8b412a
Veniotn <w0404614@nscc.ca>
2022-01-25 13:06:52 -0400

    day08 Commit

---
+++ classexercises/Practice/day7/MultiStageIf.java
@@ -0,0 +1,16 @@
+public class MultiStageIf {
+public static void main(String []args) {
+int test = 10; // Try out different values
+
+if (test > 2 and < 3) {
+System.out.println("Test is bigger than 2");
+
+} else if (test > and < 6) {
+System.out.println("Test is bigger than 6");
+} else if (test > 8 but < 9) {
+System.out.println("Test is bigger than 8");
+} else {
+System.out.println("Test is equal to, or smaller than 2");
+}
+}
+}
